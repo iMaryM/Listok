f39893b996a65563669bef22caf4d1a89b36b3c5
marym <maria_manzhos@tut.by>
2022-02-03 21:40:12 +0300

    added LogOut in TaskScreen

--- Listok/Task/TaskViewController.swift
+++ Listok/Task/TaskViewController.swift
@@ -6,14 +6,61 @@
 //
 
 import UIKit
+import Firebase
 
 class TaskViewController: UIViewController {
 
+    private lazy var logOutButton = createLogOutButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .cyan
-        // Do any additional setup after loading the view.
+        prepareUI()
     }
     
+    @objc
+    private func logOut() {
+        let firebaseAuth = Auth.auth()
+        
+        do {
+            try firebaseAuth.signOut()
+            self.navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
+    }
+
+}
+
+private extension TaskViewController {
+    
+    func prepareUI() {
+        view.backgroundColor = .white
+        view.addSubview(logOutButton)
+        pinLogOutButton()
+    }
+    
+    func createLogOutButton() -> UIButton {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        let attrs: [NSAttributedString.Key : Any] = [
+            .font : UIFont(name: "Roboto-Regular", size: 15) ?? UIFont.systemFont(ofSize: 15),
+            .foregroundColor : UIColor.white
+        ]
+        button.setAttributedTitle(NSMutableAttributedString(string: "LogOut", attributes: attrs), for: .normal)
+        button.backgroundColor = UIColor(red: 92 / 255, green: 101 / 254, blue: 202 / 255, alpha: 1)
+        button.layer.cornerRadius = 12
+        button.addTarget(self, action: #selector(logOut), for: .touchUpInside)
+        return button
+    }
+    
+    func pinLogOutButton() {
+        NSLayoutConstraint.activate([
+            logOutButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            logOutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logOutButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40),
+            logOutButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40),
+            logOutButton.heightAnchor.constraint(equalToConstant: 56)
+        ])
+    }
 }
